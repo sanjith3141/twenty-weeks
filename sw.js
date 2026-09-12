ef001189527889ca1efb3236e03b4af4008300ec
sanjith3141 <sanjith1.618@gmail.com>
2026-09-12 11:18:38 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'twentyweeks-v1';
+const CACHE = 'twentyweeks-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
